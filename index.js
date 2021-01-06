5c6791560768ce52f2e580591e602f4041b073f3
devbearu <mail@devbear.ru>
2021-01-06 17:48:53 +0300

    TODO-приложение с использованием express, mongoDB, express-handlebars

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routes/todos');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = exphbs.create({
+	defaultLayout: 'main',
+	extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(todoRoutes);
+
+async function start() {
+	try {
+		await mongoose.connect(
+			'mongodb+srv://devbear:1q2w3e4r@cluster0.etgks.mongodb.net/todos-list',
+			{
+				useNewUrlParser: true,
+				useFindAndModify: false,
+				useUnifiedTopology: true
+			}
+		);
+		app.listen(PORT, () => {
+			console.log('Server has been started...');
+		});
+	} catch (e) {
+		console.log(e);
+	}
+};
+
+start();
